4e3b9299c5dc04bd419f23a360dc209abfbb8055
rbaekdev <Richard@rbaek.dev>
2022-02-17 23:08:38 -0600

    chapter 2

--- projects/2-GuessingGame/game/src/main.rs
+++ projects/2-GuessingGame/game/src/main.rs
@@ -1,12 +1,20 @@
 // import standard input output library
 // :: similar to object dot notation  ex: std.io , String.new()
+// :: but probably similar to scope resolution
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+// $ cargo doc --open 
+// to open docs for crates
 
 // entry function main
 fn main() {
     println!("Guess the number!");
     
-    println!("What is your guess:");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("What is your guess:");
 
     /* create variable 
     - immutable by default 
@@ -18,11 +26,41 @@ fn main() {
     // const SECONDS_THREE_HOURS: u32 = 360 * 3;
 
     // if std::io was not imported the next line would be written as std::io::stdin instead
+    /*
+    The & indicates that this argument is a reference, 
+    which gives you a way to let multiple parts of your code access one piece 
+    of data without needing to copy that data into memory multiple times.
+    references are immutable by default as well
+    hence the mut
+    */
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     // dot notation here - need to come back to explain how it is different from ::
     // create gh Issue #1
     
-    println!("Your guess: {}", guess);
+    //Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables
+    //This feature is often used when you want to convert a value from one type to another type.
+    //The colon (:) after guess tells Rust we’ll annotate the variable’s type.
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        // {} is a placeholder
+    // multi {} example - println!("x = {} and y = {}", x, y);
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
+
+// $ cargo update - ignore lock file to update crates minor version
