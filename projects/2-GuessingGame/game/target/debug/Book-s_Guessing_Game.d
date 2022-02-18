2084c7549d35c496ccf0b8a3d0de1882a5a69467
Richard Baek <59379903+rbaekdev@users.noreply.github.com>
2022-02-17 20:34:55 -0600

    Delete projects/2-GuessingGame/game/target directory

--- projects/2-GuessingGame/game/target/debug/Book-s_Guessing_Game.d
+++
@@ -1,1 +0,0 @@
-D:\git\rust_book\projects\2-GuessingGame\game\target\debug\Book-s_Guessing_Game.exe: D:\git\rust_book\projects\2-GuessingGame\game\src\main.rs
--- projects/2-GuessingGame/game/target/debug/deps/Book_s_Guessing_Game.d
+++
@@ -1,5 +0,0 @@
-D:\git\rust_book\projects\2-GuessingGame\game\target\debug\deps\Book_s_Guessing_Game.exe: src\main.rs
-
-D:\git\rust_book\projects\2-GuessingGame\game\target\debug\deps\Book_s_Guessing_Game.d: src\main.rs
-
-src\main.rs:
